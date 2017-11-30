71f0887134ca92a2ce5fe1e16e679040b427d028
kipphr <kipphr@rose-hulman.edu>
2017-11-30 11:40:07 -0500

    finished m2e & m3 & m6 & m7. progress m8.

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -112,7 +112,7 @@ def draw_circles2():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement this function, per its doc-string above.
+    # done: 5. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # ------------------------------------------------------------------
@@ -120,6 +120,20 @@ def draw_circles2():
     print('--------------------------------------------------')
     print('Running draw_circles2:  See graphics window')
     print('--------------------------------------------------')
+
+    window = rg.RoseWindow(400, 400)
+
+    for k in range(18):
+        m = 50 + 20*k
+        center = rg.Point(m, 200)
+        circle = rg.Circle(center, 10)
+        circle.fill_color = 'blue'
+        circle.attach_to(window)
+        window.render(0.02)
+
+
+
+    window.close_on_mouse_click()
 
 
 def print_sequence3():
